ba9887013601a5f1259ba373523eea0c933d5ed5
Luke 10x <spam@luke10x.dev>
2020-09-01 20:32:11 +0300

    🚧 Calculates stats of all loaded pages

--- frontend/src/logviewer/reducer.test.ts
+++ frontend/src/logviewer/reducer.test.ts
@@ -29,3 +29,44 @@ describe('Book is empty', () => {
     });
   });
 });
+
+describe('book has one page already', () => {
+  const someBook: Book = {
+    pages: [
+      {
+        lines: [
+          { date: '2020-02-02 20:20:20,222', severity: 'WARN', message: 'Some warning' },
+          { date: '2020-02-02 20:20:20,222', severity: 'INFO', message: 'Some info' },
+        ],
+        lineCount: 2,
+        start: 0,
+        end: 1023,
+      },
+    ],
+    stats: { WARN: 1, INFO: 1 },
+  };
+
+  describe('another page added', () => {
+    const page: CountedPage = {
+      lines: [
+        { date: DATE, severity: 'WARN', message: 'Some warning 2' },
+        { date: DATE, severity: 'WARN', message: 'Some warning 3' },
+        { date: DATE, severity: 'EMERG', message: 'Some info' },
+      ],
+      lineCount: 3,
+      start: 1024,
+      end: 2048,
+    };
+
+    const newBook = bookReducer(someBook, page);
+
+    it('adds that page to the book', () => {
+      expect(newBook.pages).toHaveLength(2);
+      expect(newBook.pages[0].lines).toHaveLength(2);
+      expect(newBook.pages[1].lines).toHaveLength(3);
+      expect(newBook.stats.WARN).toBe(3);
+      expect(newBook.stats.INFO).toBe(1);
+      expect(newBook.stats.EMERG).toBe(1);
+    });
+  });
+});
--- frontend/src/logviewer/reducer.ts
+++ frontend/src/logviewer/reducer.ts
@@ -1,4 +1,4 @@
-import { CountedPage, Stats, Line } from "./page";
+import { CountedPage, Stats, Line } from './page';
 
 export interface Book {
   pages: CountedPage[];
@@ -10,18 +10,15 @@ export const bookReducer = (book: Book, page: CountedPage): Book => {
   if (pageIndex === -1) {
     const pages = [...book.pages, page];
 
-    const stats: Stats = page.lines.reduce<Stats>(
-      (acc: Stats, line: Line) => {
-        const newStats = { ...acc };
-        const key = line.severity;
-        newStats[key] = (newStats[key] | 0) + 1;
-        return newStats;
-      },
-      {}
-    );
+    const stats: Stats = page.lines.reduce<Stats>((acc: Stats, line: Line) => {
+      const newStats = { ...acc };
+      const key = line.severity;
+      newStats[key] = (newStats[key] | 0) + 1;
+      return newStats;
+    }, book.stats);
     return { ...book, pages, stats };
   }
-  
+
   console.error('cannot replace page yet, page: ', page);
   return book;
-}
+};
